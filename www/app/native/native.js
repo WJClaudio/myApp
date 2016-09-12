ef2e0b3df9d3e66ffbd9a4e63bcf506e1627838f
Claudio <claudio@webjunto.com>
2016-09-12 15:18:45 -0400

    google maps plugin

--- www/app/native/native.js
+++ www/app/native/native.js
@@ -10,7 +10,58 @@ angular.module('app')
   });
 })
 
-.controller('NativeCtrl', function($scope, $state, $rootScope) {
+.controller('NativeCtrl', function($scope, $state, $rootScope, $cordovaGeolocation) {
   console.log("Inside NativeCtrl");
+
+  var options = {timeout: 10000, enableHighAccuracy: true};
+
+  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+
+  $scope.dropMarkerClick = function() {
+    console.log("I'm being pressed!");
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+      var ll = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var marker = new google.maps.Marker({
+        map: $scope.map,
+        animation: google.maps.Animation.DROP,
+        position: ll
+      });
+
+    }, function(error){
+      console.log("Could not get location");
+    });
+  };
+
+  $scope.centerOnMeClick = function() {
+
+    console.log("Center on me!");
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+      var ll = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      $scope.map.panTo(ll);
+
+    }, function(error){
+      console.log("Could not get location");
+    });
+  }
 
 });
